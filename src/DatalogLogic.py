e5fe12791c0b1a742581b8fd9064e5ba8557343a
DESKTOP-AHD9183\RebeccaS <rebecca.sigmund@web.de>
2019-09-17 13:14:17 +0200

    add functionality to add a new recipe

--- src/DatalogLogic.py
+++ src/DatalogLogic.py
@@ -1,6 +1,16 @@
 from pyDatalog import pyDatalog
 import mysql.connector
 
+def getCaloriesPer100g(recipeName):
+    calorieSum = 0
+    calories = pyDatalog.ask("baseIngredientWithCalories('" + recipeName + "', X, Y)")
+    if not calories == None:
+        for c in calories.answers:
+            calorieSum += c[1]
+        return calorieSum * 100 / pyDatalog.ask("weightPerServing('" + recipeName + "', X)").answers[0][0]
+    else:
+        return 0
+
 @pyDatalog.program()
 def init_datalog():
     mydb = mysql.connector.connect(
@@ -9,6 +19,9 @@ def init_datalog():
         passwd="test",
         database="recipeDatabase"
     )
+    if not mydb.is_connected():
+        print("There is a problem with the Database")
+        raise Exception("Database Error")
     mycursor = mydb.cursor()
 
     # create all Datalog terms
@@ -49,13 +62,14 @@ def init_datalog():
     mycursor.execute("SELECT * FROM `Rezepte`")
     myresult = mycursor.fetchall()
     for x in myresult:
+        pyDatalog.assert_fact('hasID', x[1], "recipe", "recipe" + str(x[0]))
         pyDatalog.assert_fact('recipeInstructions', x[1], x[2])
         ingredients = pyDatalog.ask("containsIngredient('" + x[1] + "', Y, Z)")
         weight = 0
-        for i in ingredients.answers:
-            weight += i[1]
+        if not ingredients == None:
+            for i in ingredients.answers:
+                weight += i[1]
         pyDatalog.assert_fact('weightPerServing', x[1], weight)
-        pyDatalog.assert_fact('hasID', x[1], "recipe", "recipe" + str(x[0]))
 
     # resolve all ingredients from a recipe
     pyDatalog.create_terms('A, B, C, D, X, Y, Z, I, containsBaseIngredient')
@@ -70,12 +84,7 @@ def init_datalog():
     # hasCaloriesPer100g2(X, Y) <= baseIngredientWithCalories(X, A, B) & Y == sum(B, for_each=X))
     recipes = pyDatalog.ask('recipeInstructions(X, Y)')
     for r in recipes.answers:
-        calorieSum = 0
-        calories = pyDatalog.ask("baseIngredientWithCalories('" + r[0] + "', X, Y)")
-        for c in calories.answers:
-            calorieSum += c[1]
-        calorieSum = calorieSum * 100 / pyDatalog.ask("weightPerServing('" + r[0] + "', X)").answers[0][0]
-        pyDatalog.assert_fact('hasCaloriesPer100g', r[0], calorieSum)
+        pyDatalog.assert_fact('hasCaloriesPer100g', r[0], getCaloriesPer100g(r[0]))
 
     # resolve allergens for recipes
     containsGluten(X) <= containsIngredient(X, A, B) & containsGluten(A)
@@ -83,6 +92,9 @@ def init_datalog():
     containsMeat(X) <= containsIngredient(X, A, B) & containsMeat(A)
     containsAnimalProduct(X) <= containsIngredient(X, A, B) & containsAnimalProduct(A)
 
+    if mydb.is_connected():
+        mycursor.close()
+        mydb.close()
 
 def getRecipesOrIngredients(recipeOrIngredient, glutenFree, lactoseFree, vegan, vegetarian, lowCalorie):
     query = "hasID(X,'"+ recipeOrIngredient + "', I)" + ("& ~containsGluten(X)" if glutenFree else "") + (
@@ -94,10 +106,83 @@ def getRecipesOrIngredients(recipeOrIngredient, glutenFree, lactoseFree, vegan, 
 def getRecipeDetails(recipeID):
     recipeInstructions = pyDatalog.ask("hasID(X, 'recipe', '" + recipeID + "') & recipeInstructions(X, Y)")
     recipeIngredients = pyDatalog.ask("containsIngredient('" + recipeInstructions.answers[0][0] + "', Y, Z)")
-    return recipeInstructions.answers + recipeIngredients.answers
+    recipeIngredientsReturnValue = []
+    if not recipeIngredients == None:
+        recipeIngredientsReturnValue = recipeIngredients.answers
+    return recipeInstructions.answers + recipeIngredientsReturnValue
 
 def isValidId(id):
     return pyDatalog.ask("hasID(X, Y, '" + id + "')")
 
+def addRecipe(name, ingredients, instructions):
+    isAlreadyDefined = pyDatalog.ask("hasID('" + name + "', 'recipe', Z)")
+    if (not isAlreadyDefined == None):
+        print("This recipe is already defined")
+        return isAlreadyDefined.answers[0][0]
+
+    # insert recipe in database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="user",
+        passwd="test",
+        database="recipeDatabase"
+    )
+    mycursor = mydb.cursor()
+
+    sqlRezepte = "INSERT INTO `Rezepte` (`Name`, `Anleitung`) VALUES (%s, %s)"
+    valRezepte = [(name, instructions)]
+    mycursor.executemany(sqlRezepte, valRezepte)
+    mydb.commit()
+    recipeNumber = mycursor.lastrowid
+    id = "recipe" + str(recipeNumber)
+
+    # add recipe to datalog
+    pyDatalog.assert_fact('hasID', name, "recipe", id)
+    pyDatalog.assert_fact('recipeInstructions', name, instructions)
+
+    # insert ingredients in database
+    for i in ingredients:
+        if isValidId(i[0]):
+            if "ingredient" in i[0]:
+                sqlEnthaeltLebensmittel = "INSERT INTO `enthaeltLebensmittel` (`RezeptId`, `LebensmittelId`, `Menge`) VALUES (%s, %s, %s)"
+                valEnthaeltLebensmittel = [
+                    (recipeNumber, int(i[0][10:11]), i[1])
+                ]
+                mycursor.executemany(sqlEnthaeltLebensmittel, valEnthaeltLebensmittel)
+                mydb.commit()
+            if "recipe" in i[0]:
+                sqlEnthaeltrezept = "INSERT INTO `enthaeltRezept` (`RezeptId`, `KomponentenRezeptId`, `Menge`) VALUES (%s, %s, %s)"
+                valEnthaeltrezept = [
+                    (recipeNumber, int(i[0][6:7]), i[1])
+                ]
+                mycursor.executemany(sqlEnthaeltrezept, valEnthaeltrezept)
+                mydb.commit()
+
+            # add ingredient to datalog
+            ingredient = pyDatalog.ask(("hasID(X, Y, '" + i[0] + "')"))
+            pyDatalog.assert_fact('containsIngredient', name, ingredient.answers[0][0], i[1])
+        else:
+            print(i[0] + " is no valid id")
+
+    # add weight to datalog
+    ingredients = pyDatalog.ask("containsIngredient('" + name + "', Y, Z)")
+    weight = 0
+    if not ingredients == None:
+        for i in ingredients.answers:
+            weight += i[1]
+    pyDatalog.assert_fact('weightPerServing', name, weight)
+    pyDatalog.assert_fact('hasCaloriesPer100g', name, getCaloriesPer100g(name))
+
+    if mydb.is_connected():
+        mycursor.close()
+        mydb.close()
+    return id
+
 #print(getRecipesOrIngredients("ingredient", True, True, True, True, True))
 #print(getRecipeDetails("recipe1"))
+
+#print(addRecipe("RecipeName", [("ingredient1", 100), ("ingredient5", 20), ("recipe1", 200)], "Instruction"))
+#print(getRecipesOrIngredients("recipe", False, False, False, False, False))
+#print(getRecipeDetails("recipe4"))
+#print(pyDatalog.ask("hasCaloriesPer100g(Y, Z)"))
+#print(pyDatalog.ask("containsGluten(Y)"))
--- src/UserInterface.py
+++ src/UserInterface.py
@@ -1,9 +1,15 @@
-from src.DatalogLogic import getRecipesOrIngredients, getRecipeDetails, isValidId
+import sys
+
+from src.DatalogLogic import getRecipesOrIngredients, getRecipeDetails, isValidId, addRecipe
 
 shouldExit = False
 
 def printRecipeList(glutenFree, lactoseFree, vegan, vegetarian, lowCalorie):
     print(getRecipesOrIngredients("recipe", glutenFree, lactoseFree, vegan, vegetarian, lowCalorie))
+
+def printAllIngredients():
+    print("\nVerfügbare Zutaten sind:")
+    print(getRecipesOrIngredients("ingredient", False, False, False, False, False))
 
 def printRecipe(id):
     if isValidId(id) == None:
@@ -17,16 +23,43 @@ while not shouldExit:
     i = input("\nWelche Kriterien sollen die Rezepte erfüllen? ")
     if "exit" in i:
         shouldExit = True
-        break
+        sys.exit(0)
     printRecipeList("glutenfrei" in i, "lactosefrei" in i, "vegan" in i, "vegetarisch" in i, "kalorienarm" in i)
-    i = input("\nSoll ein Rezept angezeigt weden (ja / nein) ?")
-    if "exit" in i:
+
+    rezeptAnzeigen = input("\nSoll ein Rezept angezeigt weden (ja / nein) ?")
+    if "exit" in rezeptAnzeigen:
         shouldExit = True
-        break
-    while "ja" in i:
+        sys.exit(0)
+    while "ja" in rezeptAnzeigen:
         id = input("\nBitte geben Sie die ID des Rezepts ein (z.B. recipe1): ")
         printRecipe(id)
-        i = input("\nSoll ein weiteres Rezept angezeigt weden (ja / nein)? ")
-        if "exit" in i:
+        rezeptAnzeigen = input("\nSoll ein weiteres Rezept angezeigt weden (ja / nein)?  ")
+        if "exit" in rezeptAnzeigen:
             shouldExit = True
-        break
+            sys.exit(0)
+
+    rezeptAnlegen = input("\nSoll ein Rezept neu angelegt werden (ja / nein) ? ")
+    if "exit" in rezeptAnlegen:
+        shouldExit = True
+        sys.exit(0)
+    while "ja" in rezeptAnlegen:
+        name = input("\nBitte geben Sie den Namen des Rezepts ein: ")
+        ingredients = []
+        printAllIngredients()
+        zutatHinzufuegen = input("\nSoll eine Zutat hinzugefuegt werden (ja / nein) ? ")
+        while "ja" in zutatHinzufuegen:
+            zutatId = input("\nBitte geben Sie die id der Zutat ein (z.B. ingredient1): ")
+            if isValidId(zutatId):
+                zutatMenge = float(input("\nBitte geben Sie die Menge der Zutat in Gramm ein (z.B. 40): "))
+                ingredients.append((zutatId, zutatMenge))
+            else:
+                print(zutatId + " + ist keine mögliche ID")
+            zutatHinzufuegen = input("\nSoll eine Zutat hinzugefuegt werden (ja / nein) ? ")
+        instructions = input("\nBitte geben Sie eine Anleitung für das Rezept ein: ")
+        id = addRecipe(name, ingredients, instructions)
+        print("\nRezept wurde gespeichert: ")
+        printRecipe(id)
+        rezeptAnlegen = input("\nSoll ein weiteres Rezept angelegt werden (ja / nein)? ")
+        if "exit" in rezeptAnlegen:
+            shouldExit = True
+            sys.exit(0)
