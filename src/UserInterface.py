ddfa7ecf416a7ace4a97e5e55ba30f915acad0af
DESKTOP-AHD9183\RebeccaS <rebecca.sigmund@web.de>
2019-09-17 03:00:26 +0200

    Added some exit points to UserInterface

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -2,13 +2,13 @@ from src.DatalogLogic import getRecipesOrIngredients, getRecipeDetails, isValidI
 
 shouldExit = False
 
-def getRecipeListAsJson(glutenFree, lactoseFree, vegan, vegetarian, lowCalorie):
-    return getRecipesOrIngredients("recipe", glutenFree, lactoseFree, vegan, vegetarian, lowCalorie)
+def printRecipeList(glutenFree, lactoseFree, vegan, vegetarian, lowCalorie):
+    print(getRecipesOrIngredients("recipe", glutenFree, lactoseFree, vegan, vegetarian, lowCalorie))
 
-def getRecipeAsJson(id):
+def printRecipe(id):
     if isValidId(id) == None:
-        return "This is no valid ID"
-    return getRecipeDetails(id)
+        print("This is no valid ID")
+    print(getRecipeDetails(id))
 
 
 print("Willkommen im Rezeptverzeichnis.")
@@ -18,9 +18,15 @@ while not shouldExit:
     if "exit" in i:
         shouldExit = True
         break
-    print(getRecipeListAsJson("glutenfrei" in i, "lactosefrei" in i, "vegan" in i, "vegetarisch" in i, "kalorienarm" in i))
+    printRecipeList("glutenfrei" in i, "lactosefrei" in i, "vegan" in i, "vegetarisch" in i, "kalorienarm" in i)
     i = input("\nSoll ein Rezept angezeigt weden (ja / nein) ?")
+    if "exit" in i:
+        shouldExit = True
+        break
     while "ja" in i:
-        id = input("\nBitte geben Sie die ID des Rezepts ein: ")
-        print(getRecipeAsJson(id))
+        id = input("\nBitte geben Sie die ID des Rezepts ein (z.B. recipe1): ")
+        printRecipe(id)
         i = input("\nSoll ein weiteres Rezept angezeigt weden (ja / nein)? ")
+        if "exit" in i:
+            shouldExit = True
+        break
